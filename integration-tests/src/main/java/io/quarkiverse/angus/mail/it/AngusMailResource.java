9f05d2cac25af319fcbd265d844859578e5cfa00
George Gastaldi <gegastaldi@gmail.com>
2025-01-13 11:20:00 -0300

    Improved tests

--- integration-tests/src/main/java/io/quarkiverse/angus/mail/it/AngusMailResource.java
+++ integration-tests/src/main/java/io/quarkiverse/angus/mail/it/AngusMailResource.java
@@ -1,22 +1,26 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.quarkiverse.angus.mail.it;
 
+import java.util.Properties;
+
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.mail.Session;
+import jakarta.mail.internet.MimeMessage;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 
@@ -26,7 +30,11 @@ public class AngusMailResource {
     // add some rest methods here
 
     @GET
-    public String hello() {
+    public String hello() throws Exception {
+        Properties props = new Properties();
+        Session session = Session.getInstance(props);
+        MimeMessage message = new MimeMessage(session);
+        message.setText("Hello angus-mail");
         return "Hello angus-mail";
     }
 }
